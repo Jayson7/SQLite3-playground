4aefe405788a9e78ea23ebfe66f345b2b355b0d1
jayson7 <lexxiijoo70@gmail.com>
2021-12-08 17:45:13 +0100

    sqlite3

--- one/sqliteone.py
+++ one/sqliteone.py
@@ -144,6 +144,21 @@ c = connection.cursor()
 #     result = t.format(i[0], i[1], i[2], i[3] )
 #     print(result)
 
+# delete a data**************************************
+c.execute("DELETE   customersinfo  WHERE rowid= 5 ")
+c.execute("SELECT rowid, * FROM    customersinfo")
+
+print("done")
+databases = c.fetchall()
+
+print("NAME" + '\t LASTNAME ' + "\t PHONE NUMBER ")  
+print("-----------------" + '\t-------------------')
+
+for i in databases:
+    # num = str(i[2])
+    t =  "{}. {}  -  {} \t {}"
+    result = t.format(i[0], i[1], i[2], i[3] )
+    print(result)
 
 
 connection.commit() #push to database
